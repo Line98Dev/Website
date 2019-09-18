c4c3cfbcc0066dd50f1ac2e0debd617949c6f062
Hunter Line <hunter@tline.dev>
2019-09-17 21:03:03 -0400

    Added Other Projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,7 +7,19 @@ export function Projects(){
             <h4><a href="https://hunter.tline.dev">This Website</a></h4>
             <ul>
                 <li>This website is created with React, hosted with AWS Amplify, DNS handled by Cloudflare.</li>
-                <li>The goal of this site is to </li>
+                <li>The goal of this site is to learn new technologies.</li>
+            </ul>
+            <h4><a href="https://heyya.app">Hey Ya!</a></h4>
+            <ul>
+                <li>Created for a class. Made with a group of 4 people.</li>
+                <li>Instant Messenger using Google Firebase built with React.</li>
+                <li>Handle authentication using 3rd party tools.</li>
+                <li>Implement data storage and data security.</li>
+            </ul>
+            <h4><a href="https://nlsoftworks.com">NLSoftworks</a></h4>
+            <ul>
+                <li>Mange the local server and work on adding new features.</li>
+                <li>Research new technologies and how they can benefit the goals.</li>
             </ul>
         </div>
     )
