e630f9e66a4b9a3167924c03d16af00d3a107885
Hunter Line <htline@bsu.edu>
2020-05-31 13:53:37 -0400

    Navigation change
    Changed tabs to navigation elements
    Added favicon and updated site manifest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import {Container, Tab, Tabs} from "react-bootstrap";
+import {Container, Nav} from "react-bootstrap";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+    NavLink
+} from "react-router-dom";
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -23,20 +30,38 @@ function App() {
                 <Container>
                     <h1>Hunter Line</h1>
                     <h3>Junior Developer</h3>
-                    <Tabs justify defaultActiveKey="about" id="uncontrolled-tab-example">
-                        <Tab eventKey="about" title="About">
-                            <About/>
-                        </Tab>
-                        <Tab eventKey="education" title="Education">
-                            <Education/>
-                        </Tab>
-                        <Tab eventKey="projects" title="Projects">
-                            <Projects/>
-                        </Tab>
-                        <Tab eventKey="contact" title="Contact">
-                            <Contact/>
-                        </Tab>
-                    </Tabs>
+                    <Router history={""}>
+                        <Nav justify variant="tabs">
+                            <Nav.Item>
+                                <Nav.Link as={NavLink} exact={true} activeClassName='active' to={"/"}>About</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link as={NavLink} activeClassName='active' to={"/education"}>Education</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link as={NavLink} activeClassName='active' to={"/projects"}>Projects</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link as={NavLink} activeClassName='active' to={"/contact"}>Contact</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+
+                        <Switch>
+                            <Route exact path={"/"}>
+                                <About/>
+                            </Route>
+                            <Route path={"/education"}>
+                                <Education/>
+                            </Route>
+                            <Route path={"/projects"}>
+                                <Projects/>
+                            </Route>
+                            <Route path={"/contact"}>
+                                <Contact/>
+                            </Route>
+                            <Redirect to="/" />
+                        </Switch>
+                    </Router>
                 </Container>
             </main>
         </div>
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../App.css';
+import Card from "react-bootstrap/Card";
 
 function Education() {
     return (
         <div>
             <h2>Education</h2>
+            <Card border="danger" className="bg-dark text-white" fluid >
+                <Card.Img src="https://www.bsu.edu/-/media/www/images/home/banner/summershafertower.jpg" alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title>Ball State University</Card.Title>
+                    <Card.Text className={"shadow p-3 mb-5"}>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.ImgOverlay>
+            </Card>
         </div>
     );
 }
